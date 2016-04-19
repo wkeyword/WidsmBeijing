84536d505c897fa358ee3c40aca2688d24046881
changzheng <909459046@qq.com>
2016-04-19 21:54:06 +0800

    修复R文件编译问题

--- app/src/main/java/com/changzheng/widsmbeijing/activity/MainActivity.java
+++ app/src/main/java/com/changzheng/widsmbeijing/activity/MainActivity.java
@@ -1,15 +1,16 @@
 package com.changzheng.widsmbeijing.activity;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.changzheng.widsmbeijing.R;
 
-public class GuideActivity extends BaseFragmentActivity {
+public class MainActivity extends BaseFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.activity_guide);
     }
 }
